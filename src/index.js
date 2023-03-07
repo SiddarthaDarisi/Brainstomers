9761cb5ceba9ae48496082029a3c0d3ebe06b7c4
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-03-07 13:06:05 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {amplify} from 'aws-amplify';
+import { Amplify } from 'aws-amplify'
 import config from './aws-exports';
-amplify.configure(config);
 import { AmplifyProvider } from '@aws-amplify/ui-react'
-
 import '@aws-amplify/ui-react/styles.css'
 
+Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AmplifyProvider>
     <App />
-  </React.StrictMode>
+  </AmplifyProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
