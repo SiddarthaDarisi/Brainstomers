a02a0445a7448afee2c16dfae7ab9cebdde99f73
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-03-08 05:20:37 -0500

    simple changes

--- src/App.js
+++ src/App.js
@@ -5,34 +5,12 @@ import Curriculum from './compon/curriculum.js';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import React from 'react';
-import logo from './logo.png';
+
+import Topbar from "./compon/topbar.js"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <nav class="navbar">
-                <Link to="/">
-                <img src={logo}className="App-logo" alt="logo"/>
-                </Link>
-                 
-                <div id="rnav">
-                                
-                  <div id="pricing">
-                    <Link to ="/pricing" className="btn btn-primary"style={{ textDecoration: 'none' }} id="pricing-text">Pricing</Link>
-                  </div>
-                  <div id="curriculum">
-                     <Link to ="/curriculum" className="btn btn-primary"style={{ textDecoration: 'none' }} id="curriculum-text">Curriculum</Link>
-                  </div>
-                  <div id="login">
-                      <Link to ="/login" className="btn btn-primary"style={{ textDecoration: 'none' }} id="login-text">Login</Link>
-                  </div>
-                </div>                     
-        
-              
-         
-        </nav>
-      </header>
+      <Topbar/>
 
        <Routes>
         <Route path="/" element={<Landing />} />
--- src/compon/landing.js
+++ src/compon/landing.js
@@ -1,12 +1,15 @@
-
+import Backgroundimg from './background.jpg'
 import React from 'react';  
-
 import "./landing.css";
+
 function landing() {
     return (
         <div className="landing">
+            <div className='topp' >
+            <img src={Backgroundimg} alt="background" id="backgroundimg" />
             <p id="tagline" >Learn faster through <br/>structured training</p>
             <button id="getstarted">Get started</button>
+            </div>
         </div>
     );
 };
