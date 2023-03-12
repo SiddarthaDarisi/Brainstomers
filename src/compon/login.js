71ce9ad7187a04a0c2ef3f886b136fe949d4db33
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-03-12 14:41:37 -0400

    small change

--- src/compon/login.js
+++ src/compon/login.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { ReactNode, useState } from 'react';
 import Dashboard from './Dashboard';
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Navigate } from 'react-router-dom';
 
 const login = () => {
 
-    return (
-        <>
+  return (
+    <>
 
-            <Dashboard />
-        </>
-    );
+      <Dashboard />
+    </>
+  );
 
 }
 export default withAuthenticator(login);
